c587b0f1774fa42a0d62dbca4cbbe7734706cf1e
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-07-15 15:12:10 +0300

    clean up

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -3,19 +3,18 @@ import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 const Template = ({ children, location })=> (
   <div className="container">
-    <div className="pages">
-      <ReactCSSTransitionGroup
-        component="div"
-        transitionName="top"
-        transitionEnterTimeout={500}
-        transitionLeaveTimeout={500}>
+    <ReactCSSTransitionGroup
+      component="div"
+      className="pages"
+      transitionName="top"
+      transitionEnterTimeout={500}
+      transitionLeaveTimeout={500}>
 
-        {React.cloneElement(children, {
-          key: location.pathname
-        })}
-
-      </ReactCSSTransitionGroup>
-    </div>
+      {React.cloneElement(children, {
+        key: location.pathname
+      })}
+      
+    </ReactCSSTransitionGroup>
   </div>
 )
 
