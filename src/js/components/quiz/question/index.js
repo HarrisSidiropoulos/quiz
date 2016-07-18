65facb8d1812262e8c7b0c0151806e628c1d161e
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-07-18 14:28:30 +0300

    clean up

--- src/js/components/quiz/question/index.js
+++ src/js/components/quiz/question/index.js
@@ -5,22 +5,22 @@ import Popover from 'react-bootstrap/lib/Popover'
 import QUIZ_DATA from 'data'
 import acc from 'utils/acc'
 
-const images = [
-  require('./images/Q1.jpg'),
-  require('./images/Q2.jpg'),
-  require('./images/Q3.jpg'),
-  require('./images/Q4.jpg'),
-  require('./images/Q5.jpg'),
-  require('./images/Q6.jpg'),
-  require('./images/Q7.jpg')
-];
+const images = {
+  "Q1.jpg":require('./images/Q1.jpg'),
+  "Q2.jpg":require('./images/Q2.jpg'),
+  "Q3.jpg":require('./images/Q3.jpg'),
+  "Q4.jpg":require('./images/Q4.jpg'),
+  "Q5.jpg":require('./images/Q5.jpg'),
+  "Q6.jpg":require('./images/Q6.jpg'),
+  "Q7.jpg":require('./images/Q7.jpg')
+};
 
 const artists = {
-  'Μιχάλης Κουνέλης':require('./images/kounelis-mixalis.jpg'),
-  'Γιώργος Κουτσουρέλης':require('./images/koytsourelis-giorgos.jpg'),
-  'Κωνσταντίνος Παπαδάκης ή Ναύτης':require('./images/papadakis-kostas.jpg'),
-  'Νικόλαος Τσέγκας':require('./images/tsagas-nikolaos.jpg'),
-  'Νικόλαος Χάρχαλης':require('./images/xarxalis-nikolaos.jpg')
+  "Μιχάλης Κουνέλης":require('./images/kounelis-mixalis.jpg'),
+  "Γιώργος Κουτσουρέλης":require('./images/koytsourelis-giorgos.jpg'),
+  "Κωνσταντίνος Παπαδάκης ή Ναύτης":require('./images/papadakis-kostas.jpg'),
+  "Νικόλαος Τσέγκας":require('./images/tsagas-nikolaos.jpg'),
+  "Νικόλαος Χάρχαλης":require('./images/xarxalis-nikolaos.jpg')
 };
 
 class Question extends Component {
@@ -50,7 +50,6 @@ class Question extends Component {
       dispatch, question, totalQuestions, currentQuestion, answers, image
     } = this.props;
 
-    const img = parseInt(image.replace('Q', ''), 10) - 1;
     const _answers = answers.map((item, index)=> {
       item.text = QUIZ_DATA.letters[index].toUpperCase() + '. ' +item.label;
       if (item.answered && item["is-correct"]) {
@@ -72,7 +71,7 @@ class Question extends Component {
         </div>
         <div className="right-panel">
           <div className="image-container">
-            <img src={images[img]} alt={img} width="560" height="555" />
+            <img src={images[image]} alt={image} width="560" height="555" />
           </div>
         </div>
       </div>
