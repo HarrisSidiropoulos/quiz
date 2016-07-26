2c664f7a0d6137992b64b3112026339c49a28226
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-07-26 12:34:52 +0300

    offline update component

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import OfflineUpdate from './offline-update'
 
 const Template = ({ children, location })=> (
-  <div className="container">
-    <ReactCSSTransitionGroup
-      component="div"
-      className="pages"
-      transitionName="top"
-      transitionAppear={true}
-      transitionAppearTimeout={100}
-      transitionEnterTimeout={500}
-      transitionLeaveTimeout={500}>
+  <div>
+    <OfflineUpdate />
+    <div className="container">
+      <ReactCSSTransitionGroup
+        component="div"
+        className="pages"
+        transitionName="top"
+        transitionAppear={true}
+        transitionAppearTimeout={100}
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={500}>
 
-      {React.cloneElement(children, {
-        key: location.pathname
-      })}
+        {React.cloneElement(children, {
+          key: location.pathname
+        })}
 
-    </ReactCSSTransitionGroup>
+      </ReactCSSTransitionGroup>
+    </div>
   </div>
 )
 
--- src/js/index.js
+++ src/js/index.js
@@ -3,29 +3,6 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/configureStore'
 import Root from './Root'
 
-if (process.env.NODE_ENV==='production') {
-  const {install, applyUpdate} = require('offline-plugin/runtime')
-  install({
-    onInstalled() {
-      console.log("onInstalled")
-    },
-    onUpdating() {
-      console.log("onUpdating")
-    },
-    onUpdateReady() {
-      console.log("onUpdateReady")
-      // applyUpdate()
-    },
-    onUpdateFailed() {
-      console.log("onUpdateFailed")
-    },
-    onUpdated() {
-      console.log("onUpdated")
-      // window.location.reload()
-    }
-  })
-}
-
 const store = configureStore();
 
 ReactDOM.render(
